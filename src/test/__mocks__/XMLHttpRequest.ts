00653f5f5154a0e37d6b84fa8e81ce97e21f7a56
Emir Marques <emirdeliz@gmail.com>
2023-04-10 10:30:23 -0300

    feat: Added the methodHttp option

--- src/test/__mocks__/XMLHttpRequest.ts
+++ src/test/__mocks__/XMLHttpRequest.ts
@@ -1,59 +1,59 @@
 const NETWORK_SPEED_UPLOAD = 1000000;
 
 async function processUploadRequest(
-	fileSizePending = 0,
-	uploadCallback: (fileSizePending: number) => void
+  fileSizePending = 0,
+  uploadCallback: (fileSizePending: number) => void
 ) {
-	const promisesCallback = [] as Array<Promise<void>>;
-	let fileCallbackLength = Math.round(fileSizePending / NETWORK_SPEED_UPLOAD);
-	while (fileCallbackLength > 0) {
-		fileCallbackLength--;
-		promisesCallback.push(
-			new Promise(function (resolve) {
-				const fileSizePendingUpdated =
-					fileSizePending - (5 - fileCallbackLength) * NETWORK_SPEED_UPLOAD;
-				uploadCallback(fileSizePendingUpdated);
-				setTimeout(resolve, 1000);
-			})
-		);
-	}
-	Promise.all(promisesCallback);
+  const promisesCallback = [] as Array<Promise<void>>;
+  let fileCallbackLength = Math.round(fileSizePending / NETWORK_SPEED_UPLOAD);
+  while (fileCallbackLength > 0) {
+    fileCallbackLength--;
+    promisesCallback.push(
+      new Promise(function (resolve) {
+        const fileSizePendingUpdated =
+          fileSizePending - (5 - fileCallbackLength) * NETWORK_SPEED_UPLOAD;
+        uploadCallback(fileSizePendingUpdated);
+        setTimeout(resolve, 1000);
+      })
+    );
+  }
+  Promise.all(promisesCallback);
 }
 
-export default class XMLHttpRequestMock {
-	file = {} as File;
-	requestHeader = {} as { [key: string]: string };
-	response = {} as Response;
-	upload = {} as XMLHttpRequestUpload;
+export const XMLHttpRequestBaseMock = {
+  open: jest.fn(),
+  onloadend: jest.fn(),
+  readyState: 4,
+  upload: {},
+  response: JSON.stringify({}),
+  requestHeader: {} as { [key: string]: string },
+  file: {} as File,
+  send: async function (formData: FormData) {
+    this.file = formData.get("file") as File;
 
-	onloadend = jest.fn();
-	open = jest.fn();
+    const upload = this.upload;
+    await processUploadRequest(
+      this.file.size,
+      function (fileSizePending: number) {
+        upload.onprogress && upload.onprogress(fileSizePending);
+      }
+    );
 
-	send = async function (formData: FormData) {
-		this.file = formData.get('file') as File;
+    this.response = JSON.stringify({
+      status: true,
+      message: "File is uploaded",
+      file: this.file,
+      data: {
+        name: this.file.name,
+        mimetype: this.file.type,
+        size: this.file.size,
+      },
+    });
+    this.onloadend && this.onloadend(this);
+  },
+  setRequestHeader: function (key: string, value: string) {
+    this.requestHeader[key] = value;
+  },
+};
 
-		const upload = this.upload;
-		await processUploadRequest(
-			this.file.size,
-			function (fileSizePending: number) {
-				upload.onprogress && upload.onprogress(fileSizePending);
-			}
-		);
-
-		this.response = JSON.stringify({
-			status: true,
-			message: 'File is uploaded',
-			file: this.file,
-			data: {
-				name: this.file.name,
-				mimetype: this.file.type,
-				size: this.file.size,
-			},
-		});
-		this.onloadend && this.onloadend(this);
-	};
-
-	setRequestHeader = function (key: string, value: string) {
-		this.requestHeader[key] = value;
-	};
-}
+export default () => XMLHttpRequestBaseMock;
